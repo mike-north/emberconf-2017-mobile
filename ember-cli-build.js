da63faddfcf4f8ffd532501cb8f3db13b9e1dfa1
Mike North <michael.l.north@gmail.com>
2017-04-17 15:36:31 -0700

    serviceworker webfont fix

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,29 +5,18 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     'esw-cache-fallback': {
       patterns: [
-        'https://emberconf-2017-api.mike.works/(.+)'
+        'https://emberconf-2017-api.mike.works/(.+)',
+        '/fonts/fontawesome-webfont(.+)',
       ],
     },
     'asset-cache': {
       include: [
         'assets/**/*',
+        'fonts/fontawesome-webfont.*',
         'img/**/*'
       ],
       version: '199'
     }
   });
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
   return app.toTree();
 };
